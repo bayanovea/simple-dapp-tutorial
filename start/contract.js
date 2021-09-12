4fa3e7831e040cb670f2250a8d5f0236f810177c
evgeniy.bayanov <e.bayanov@skyeng.ru>
2021-09-13 00:41:25 +0300

    Add connect and onboarding

--- start/contract.js
+++ start/contract.js
@@ -33,6 +33,49 @@ The `piggybankContract` is compiled from:
 const forwarderOrigin = 'http://localhost:9010'
 
 const initialize = () => {
-  //You will start here 
+  const onboardButton = document.getElementById('connectButton');
+  const getAccountsButton = document.getElementById('getAccounts');
+  const getAccountsResult = document.getElementById('getAccountsResult');
+
+  const isMetaMaskInstalled = () =>  {
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask)
+  }
+
+  const MetaMaskClientCheck = () => {
+    if (!isMetaMaskInstalled()) {
+      onboardButton.innerText = 'Click here to install MetaMask!';
+      onboardButton.onclick = onClickInstall;
+      onboardButton.disabled = false;
+    } else {
+      onboardButton.innerText = 'Connect';
+      onboardButton.onclick = onClickConnect;
+      onboardButton.disabled = false;
+    }
+  }
+
+  const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+
+  const onClickInstall = () => {
+    onboardButton.innerText = 'Onboarding in progress';
+    onboardButton.disabled = true;
+    onboarding.startOnboarding();
+  };
+
+  const onClickConnect = async () => {
+    try {
+      await ethereum.request({ method: 'eth_requestAccounts' });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  getAccountsButton.addEventListener('click', async () => {
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    getAccountsResult.innerHTML = accounts[0] || 'Not able to get accounts';
+  });  
+
+  MetaMaskClientCheck();
 }
+
 window.addEventListener('DOMContentLoaded', initialize)
